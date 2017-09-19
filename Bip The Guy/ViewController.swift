fb92498154389591399d5ed961ea787a4b5302bd
Jimmy McDermott <mcdappdev@gmail.com>
2017-09-19 16:03:08 -0400

    part 1

--- Bip The Guy/ViewController.swift
+++ Bip The Guy/ViewController.swift
@@ -7,19 +7,68 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    //MARK: - Outlets
+    @IBOutlet private weak var imageToPunch: UIImageView!
+    
+    //MARK: - Properties
+    private var audioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //MARK: - Functions
+    func animateImage() {
+        let bounds = imageToPunch.bounds
+        let shrinkValue: CGFloat = 60
+        
+        //shrink our imageToPunch by 60 pixels
+        imageToPunch.bounds = CGRect(
+            x: imageToPunch.bounds.origin.x + shrinkValue,
+            y: imageToPunch.bounds.origin.y + shrinkValue,
+            width: imageToPunch.bounds.size.width - shrinkValue,
+            height: imageToPunch.bounds.size.height - shrinkValue
+        )
+        
+        UIView.animate(
+            withDuration: 0.25,
+            delay: 0.0,
+            usingSpringWithDamping: 0.2,
+            initialSpringVelocity: 10,
+            options: [],
+            animations: {
+                self.imageToPunch.bounds = bounds
+        }, completion: nil)
     }
-
-
+    
+    private func playSound(soundName: String, audioPlayer: inout AVAudioPlayer) {
+        if let sound = NSDataAsset(name: soundName) {
+            do {
+                audioPlayer = try AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("Error: Couldn't be played as a sound file")
+            }
+            
+        } else {
+            print("ERROR: file \(soundName) didn't load")
+        }
+    }
+    
+    //MARK: - Actions
+    @IBAction private func photoLibraryTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction private func takePhotoTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction private func imageTapped(_ sender: UITapGestureRecognizer) {
+        animateImage()
+        playSound(soundName: "punchSound", audioPlayer: &audioPlayer)
+    }
 }
 
